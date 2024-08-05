c88897fe4f20c09670339a2c56130a48e794670f
sahyunjin <tkguswls1106@naver.com>
2024-08-06 00:20:29 +0900

    Refactor: Swagger 버전 교체 리팩토링

--- src/main/java/com/shj/onlinememospringproject/config/SwaggerConfig.java
+++ src/main/java/com/shj/onlinememospringproject/config/SwaggerConfig.java
@@ -1,68 +1,65 @@
 package com.shj.onlinememospringproject.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.*;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Configuration
-@EnableSwagger2
-public class SwaggerConfig {  // 참고로 Swagger에서 test돌릴때에 JWT 사용시, Authorize값에 'Bearer ${accessToken}'으로 넣어주면 된다.
+public class SwaggerConfig {
+
+    @Value("${server.env}")
+    private String serverEnv;
+    @Value("${server.url}")
+    private String serverUrl;
+
 
     @Bean
-    @Profile({"devswagger && !prodswagger"})  // 만약 로컬환경일경우 swagger 활성화
-    public Docket restAPI() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .useDefaultResponseMessages(false)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.shj.onlinememospringproject.controller"))
-                .paths(PathSelectors.any())
-                .build()
+    public OpenAPI openAPI() {
+        String authName = "JWT";  // 기능 타이틀명
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(authName);
 
-                .securityContexts(Arrays.asList(securityContext()))
-                .securitySchemes(Arrays.asList(apiKey()));
+        Components components = new Components()
+                .addSecuritySchemes(
+                        authName,
+                        new SecurityScheme()
+                                .name(authName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("Bearer")
+                                .bearerFormat("JWT")
+                                .description("'Bearer '을 제외한 Access Token 입력하세요.")
+                );
+
+        return new OpenAPI()
+                .addSecurityItem(securityRequirement)
+                .components(components)
+                .info(apiInfo())
+                .servers(apiServer());
     }
 
-    @Bean
-    @Profile({"prodswagger && !devswagger"})  // 만약 운영환경일경우 swagger 비활성화
-    public Docket disable() {
-        return new Docket(DocumentationType.SWAGGER_2).enable(false);
+    private Info apiInfo() {
+        return new Info()
+                .title("OnlineMemo Ver.2 - Swagger API")
+                .description("온라인메모장 대규모 리팩토링 버전입니다.")
+                .version("1.0.0");
     }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("OnlineMemo REST API")
-                .version("1.0.0")
-                .description("온라인메모장의 api 문서입니다.")
-                .build();
-    }
+    private List<Server> apiServer() {
+        String description = "Local Server";
+        if(serverEnv.equals("prod")) description = "Prod Server";
 
+        Server server = new Server()
+                .description(description)
+                .url(serverUrl);
 
-    private SecurityContext securityContext() {
-        return SecurityContext.builder()
-                .securityReferences(defaultAuth())
-                .build();
-    }
-
-    private List<SecurityReference> defaultAuth() {
-        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
-    }
-
-    private ApiKey apiKey() {
-        return new ApiKey("JWT", "Authorization", "header");
+        return Arrays.asList(server);
     }
 }
