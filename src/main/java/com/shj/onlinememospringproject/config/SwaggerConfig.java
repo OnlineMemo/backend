a0e0e5dd95e5bcbb4dc0602632308804912dc312
sahyunjin <tkguswls1106@naver.com>
2024-08-15 01:35:25 +0900

    Refactor: AWS CloudWatch 로깅방식 변경 리팩토링

--- src/main/java/com/shj/onlinememospringproject/config/SwaggerConfig.java
+++ src/main/java/com/shj/onlinememospringproject/config/SwaggerConfig.java
@@ -9,12 +9,10 @@ import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Profile("!prod")  // prod 환경이 아닐 때만 SwaggerConfig가 활성화됨.
 @Configuration
 public class SwaggerConfig {
 
@@ -26,9 +24,9 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
-//        if (serverEnv.equals("prod")) {
-//            return null;  // prod 환경일 경우 Swagger 설정을 반환하지 않음.
-//        }
+        if (serverEnv.equals("prod")) {
+            return null;  // prod 환경일 경우 Swagger 설정을 반환하지 않음. (어차피 현재는 properties에 분리해두어 사용X)
+        }
 
         String authName = "JWT";  // 기능 타이틀명
         SecurityRequirement securityRequirement = new SecurityRequirement().addList(authName);
