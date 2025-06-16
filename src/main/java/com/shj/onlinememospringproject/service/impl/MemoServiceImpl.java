051524bd42ade251ab84c3dce185a4f183d3b773
sahyunjin <tkguswls1106@naver.com>
2025-06-16 19:03:51 +0900

    [#4] Fix: 공동메모 유지시 소유자만 Lock 제거 가능

--- src/main/java/com/shj/onlinememospringproject/service/impl/MemoServiceImpl.java
+++ src/main/java/com/shj/onlinememospringproject/service/impl/MemoServiceImpl.java
@@ -277,14 +277,16 @@ public class MemoServiceImpl implements MemoService {
             memoRepository.delete(memo);  // 그 이후에 부모 테이블인 Memo에서 해당 메모를 삭제. (이후 부모 테이블인 Memo에서 삭제.)
         }
         else {  // 해당 메모가 공동메모라면
-            if(memoHasUsersCount == 2) {  // 공동메모인데, 그룹 탈퇴로 메모의 사용자가 2명에서 1명으로 개인메모가 될 경우 (즉, 원래 2명이었을 경우)
-                // 즐겨찾기 여부를 다시 0으로 초기화.
-                memoRepository.updateIsStar(memoId, 0);  // isStar 필드는 수정시각에 영향을 주지않도록, @LastModifiedDate 생명주기에서 제외시켜 따로 JPQL로 직접 업데이트함.
-            }
-
-            // 공동메모였을 경우, Redis 내 본인의 편집락 삭제 처리.
             String lockKey = "memoId:" + memoId;
-            redisRepository.unlockOwner(lockKey, loginUserId);
+            if(memoHasUsersCount == 2) {  // 공동메모인데, 그룹 탈퇴로 메모의 사용자가 2명에서 1명으로 개인메모가 될 경우 (즉, 원래 2명이었을 경우)
+                // isStar 필드는 수정시각에 영향을 주지않도록, @LastModifiedDate 생명주기에서 제외시켜 따로 JPQL로 직접 업데이트함.
+                memoRepository.updateIsStar(memoId, 0);  // 즐겨찾기 여부를 다시 0으로 초기화.
+                // 개인메모 체제로 변경되었으므로, Redis 내 편집락이 필요 없어짐.
+                redisRepository.unlock(lockKey);  // Redis 내 편집락 해제 (본인 무관)
+            }
+            else {  // 그룹을 탈퇴해도 인원수가 2명 이상 남아, 공동메모 체제를 유지할 경우
+                redisRepository.unlockOwner(lockKey, loginUserId);  // Redis 내 본인의 편집락 해제. (락 소유자만 가능)
+            }
         }
     }
 
