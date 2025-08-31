757251573730490d9fcd78668ddd7ac92c768953
sahyunjin <tkguswls1106@naver.com>
2025-08-31 12:22:51 +0900

    [#8] Feat: 특정 기간 내 실사용자 지표 API 구현

---
+++ src/main/java/com/shj/onlinememospringproject/controller/BackOfficeController.java
@@ -0,0 +1,55 @@
+package com.shj.onlinememospringproject.controller;
+
+import com.shj.onlinememospringproject.dto.Ga4FilteredDto;
+import com.shj.onlinememospringproject.response.ResponseCode;
+import com.shj.onlinememospringproject.response.ResponseData;
+import com.shj.onlinememospringproject.service.Ga4FilteredService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Tag(name = "BackOffice")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/back-office")
+public class BackOfficeController {
+
+    private final Ga4FilteredService ga4FilteredService;
+
+
+    @GetMapping("/ga4/all-data")
+    @Operation(summary = "GA4 전체 데이터 조회 [JWT O]",
+            description = """
+                    <strong>< RequestParam ></strong>
+                    - <strong>startDatetime, endDatetime</strong> : "2025-08-29 23:59:59" 형태의 날짜시각  \n
+                    <strong>< URI ></strong>
+                    - <strong>예시 URI</strong> : /back-office/ga4/all-data?startDatetime=2025-08-01%2015:30:00&endDatetime=2025-08-29%2023:59:59
+                    - <strong>참고 사항</strong> : 중간의 '%20'은 공백을 의미
+                    """)
+    public ResponseEntity<ResponseData<List<Ga4FilteredDto.Response>>> findGa4FilteredAll(
+            @RequestParam(value = "startDatetime", required = true) String startDatetime,
+            @RequestParam(value = "endDatetime", required = true) String endDatetime) {
+        List<Ga4FilteredDto.Response> responseDtoList = ga4FilteredService.findGa4FilteredAll(startDatetime, endDatetime);
+        return ResponseData.toResponseEntity(ResponseCode.READ_GA4FILTERED, responseDtoList);
+    }
+
+    @GetMapping("/ga4/calc-data")
+    @Operation(summary = "GA4 통계용 데이터 조회 [JWT O]",
+            description = """
+                    <strong>< RequestParam ></strong>
+                    - <strong>startDatetime, endDatetime</strong> : "2025-08-29 23:59:59" 형태의 날짜시각  \n
+                    <strong>< URI ></strong>
+                    - <strong>예시 URI</strong> : /back-office/ga4/calc-data?startDatetime=2025-08-01%2015:30:00&endDatetime=2025-08-29%2023:59:59
+                    - <strong>참고 사항</strong> : 중간의 '%20'은 공백을 의미
+                    """)
+    public ResponseEntity<ResponseData<List<Ga4FilteredDto.CalcResponse>>> findGa4FilteredCalc(
+            @RequestParam(value = "startDatetime", required = true) String startDatetime,
+            @RequestParam(value = "endDatetime", required = true) String endDatetime) {
+        List<Ga4FilteredDto.CalcResponse> calcResponseDtoList = ga4FilteredService.findGa4FilteredCalc(startDatetime, endDatetime);
+        return ResponseData.toResponseEntity(ResponseCode.READ_GA4FILTERED, calcResponseDtoList);
+    }
+}
