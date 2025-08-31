0b8456e72d560ed47ce6589457cafdd3f9ce2411
sahyunjin <tkguswls1106@naver.com>
2025-08-31 16:17:06 +0900

    [#8] Fix: 스케줄러 내 State 미사용 제거

--- src/main/java/com/shj/onlinememospringproject/repository/Ga4FilteredRepository.java
+++ src/main/java/com/shj/onlinememospringproject/repository/Ga4FilteredRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface Ga4FilteredRepository extends MongoRepository<Ga4Filtered, String> {  // MongoDB
 
+    Optional<Ga4Filtered> findFirstByOrderByEventDatetimeDesc();
     List<Ga4Filtered> findByEventDatetimeBetweenOrderByEventDatetimeAsc(LocalDateTime startDatetime, LocalDateTime endDatetime);
 }
--- src/main/java/com/shj/onlinememospringproject/service/Ga4FilteredScheduler.java
+++ src/main/java/com/shj/onlinememospringproject/service/Ga4FilteredScheduler.java
@@ -1,7 +1,7 @@
 package com.shj.onlinememospringproject.service;
 
-import com.shj.onlinememospringproject.domain.backoffice.State;
-import com.shj.onlinememospringproject.repository.StateRepository;
+import com.shj.onlinememospringproject.domain.backoffice.Ga4Filtered;
+import com.shj.onlinememospringproject.repository.Ga4FilteredRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -15,29 +15,22 @@ import java.time.format.DateTimeFormatter;
 public class Ga4FilteredScheduler {
 
     private final Ga4FilteredService ga4FilteredService;
-    private final StateRepository stateRepository;
+    private final Ga4FilteredRepository ga4FilteredRepository;
     private final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
 
     @Scheduled(cron = "0 0 15 * * ?", zone = "Asia/Seoul")  // 매일 오후 3시에 실행
     public void filterAndSaveGa4() {
-        State state = stateRepository.findFirstByOrderByRecentDatetimeDesc()
-                .orElseGet(() -> {
-                    State newState = State.StateSaveBuilder()
-                            .recentDatetime(null)
-                            .build();
-                    return stateRepository.save(newState);
-                });
-
         LocalDate yesterday = LocalDate.now(ZoneId.of("Asia/Seoul")).minusDays(1);
         String startDayStr;
         String endDayStr = yesterday.format(FORMATTER);  // 하루 전의 데이터까지 정제함.
 
-        if(state.getRecentDatetime() == null) {
+        Ga4Filtered recentGa4Filtered = ga4FilteredRepository.findFirstByOrderByEventDatetimeDesc().orElse(null);
+        if(recentGa4Filtered == null) {
             startDayStr = "2025-08-01";
         }
         else {
-            LocalDate recentDay = state.getRecentDatetime().toLocalDate();
+            LocalDate recentDay = recentGa4Filtered.getEventDatetime().toLocalDate();
             startDayStr = recentDay.plusDays(1).format(FORMATTER);
         }
 
@@ -46,6 +39,5 @@ public class Ga4FilteredScheduler {
         String endDatetimeStr = endDayStr + " 23:59:59";
 
         ga4FilteredService.filterAndSaveGa4(startDatetimeStr, endDatetimeStr);
-        stateRepository.updateRecentDatetimeById(state.getId(), yesterday.atTime(23, 59, 59));  // 23:59:59
     }
 }
