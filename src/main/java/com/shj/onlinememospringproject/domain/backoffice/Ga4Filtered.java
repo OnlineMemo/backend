5d306559d57a7f8c958dac48c43b479d7a404de2
sahyunjin <tkguswls1106@naver.com>
2025-08-31 12:06:16 +0900

    [#8] Feat: GA4 지표 엔티티 구현

---
+++ src/main/java/com/shj/onlinememospringproject/domain/backoffice/Ga4Filtered.java
@@ -0,0 +1,78 @@
+package com.shj.onlinememospringproject.domain.backoffice;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+
+@Document(collection = "ga4_filtered")  // 백오피스 전용 데이터는 MongoDB로 관리.
+public class Ga4Filtered {
+
+    @Id
+    private String id;
+
+    @Indexed
+    @Field("event_datetime")
+    private LocalDateTime eventDatetime;  // MongoDB 저장값: UTC, MongoDB 검색파라미터: UTC, Spring 조회결과: KST
+
+    @Field("user_pseudo_id")
+    private String userPseudoId;
+    @Field("login_user_id")
+    private Long loginUserId;
+
+    @Field("page_title")
+    private String pageTitle;
+    @Field("page_referrer")
+    private String pageReferrer;
+    @Field("page_location")
+    private String pageLocation;
+    @Field("page_path")
+    private String pagePath;
+
+    @Field("device_category")
+    private String deviceCategory;
+    @Field("device_brand")
+    private String deviceBrand;
+    @Field("device_browser")
+    private String deviceBrowser;
+
+    @Field("geo_country")
+    private String geoCountry;
+    @Field("geo_region")
+    private String geoRegion;
+    @Field("geo_city")
+    private String geoCity;
+
+
+    @Builder(builderClassName = "Ga4FilteredSaveBuilder", builderMethodName = "Ga4FilteredSaveBuilder")
+    public Ga4Filtered(
+            LocalDateTime eventDatetime,
+            String userPseudoId, Long loginUserId,
+            String pageTitle, String pageReferrer, String pageLocation, String pagePath,
+            String deviceCategory, String deviceBrand, String deviceBrowser,
+            String geoCountry, String geoRegion, String geoCity
+    ) {
+        // 이 빌더는 Ga4Filtered 생성때만 사용할 용도
+        this.eventDatetime = eventDatetime;
+        this.userPseudoId = userPseudoId;
+        this.loginUserId = loginUserId;
+        this.pageTitle = pageTitle;
+        this.pageReferrer = pageReferrer;
+        this.pageLocation = pageLocation;
+        this.pagePath = pagePath;
+        this.deviceCategory = deviceCategory;
+        this.deviceBrand = deviceBrand;
+        this.deviceBrowser = deviceBrowser;
+        this.geoCountry = geoCountry;
+        this.geoRegion = geoRegion;
+        this.geoCity = geoCity;
+    }
+}
---
+++ src/main/java/com/shj/onlinememospringproject/dto/Ga4FilteredDto.java
@@ -0,0 +1,101 @@
+package com.shj.onlinememospringproject.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.shj.onlinememospringproject.domain.backoffice.Ga4Filtered;
+import com.shj.onlinememospringproject.util.TimeConverter;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+public class Ga4FilteredDto {
+
+    // ======== < Request DTO > ======== //
+
+
+    // ======== < Response DTO > ======== //
+
+    @Getter
+    @NoArgsConstructor
+    public static class Response {
+
+        private LocalDateTime eventDatetime;
+        private String eventDatetimeStr;
+
+        private String userPseudoId;
+        private Long loginUserId;
+
+        private String pageTitle;
+        private String pageReferrer;
+        private String pageLocation;
+        private String pagePath;
+
+        private String deviceCategory;
+        private String deviceBrand;
+        private String deviceBrowser;
+
+        private String geoCountry;
+        private String geoRegion;
+        private String geoCity;
+
+        public Response(Ga4Filtered entity) {
+            this.eventDatetime = entity.getEventDatetime();
+            this.eventDatetimeStr = TimeConverter.timeToString(entity.getEventDatetime());
+            this.userPseudoId = entity.getUserPseudoId();
+            this.loginUserId = entity.getLoginUserId();
+            this.pageTitle = entity.getPageTitle();
+            this.pageReferrer = entity.getPageReferrer();
+            this.pageLocation = entity.getPageLocation();
+            this.pagePath = entity.getPagePath();
+            this.deviceCategory = entity.getDeviceCategory();
+            this.deviceBrand = entity.getDeviceBrand();
+            this.deviceBrowser = entity.getDeviceBrowser();
+            this.geoCountry = entity.getGeoCountry();
+            this.geoRegion = entity.getGeoRegion();
+            this.geoCity = entity.getGeoCity();
+        }
+    }
+
+    @Getter
+    @NoArgsConstructor
+    public static class CalcResponse {
+
+        private LocalDateTime eventDatetime;
+
+        private String userPseudoId;
+        private Long loginUserId;
+
+        private String pagePath;
+
+        public CalcResponse(Ga4Filtered entity) {
+            this.eventDatetime = entity.getEventDatetime();
+            this.userPseudoId = entity.getUserPseudoId();
+            this.loginUserId = entity.getLoginUserId();
+            this.pagePath = entity.getPagePath();
+        }
+    }
+
+    @Getter
+    @NoArgsConstructor
+    public static class ClientResponse {
+
+        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS")
+        private LocalDateTime event_datetime_kst;
+
+        private String user_pseudo_id;
+        private Long login_user_id;
+
+        private String page_title;
+        private String page_referrer;
+        private String page_location;
+        private String page_path;
+
+        private String device_category;
+        private String device_brand;
+        private String device_browser;
+
+        private String geo_country;
+        private String geo_region;
+        private String geo_city;
+    }
+}
