b8f048b4561fcb3b03e99a9731427759a43f56f5
sahyunjin <tkguswls1106@naver.com>
2025-10-03 06:20:36 +0900

    [#10] Feat: AWS predeploy 자바 종료 구현

---
+++ .platform/hooks/predeploy/kill_prev_spring.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+timestamp() {
+    date "+%Y-%m-%d %H:%M:%S.%3N"
+}
+
+if pkill -f "onlinememo-backend.jar"; then
+    echo "$(timestamp) / SUCCESS - Spring 프로세스를 성공적으로 종료했습니다."
+else
+    echo "$(timestamp) / WARN - 실행 중인 Spring 프로세스가 없어 종료하지 못했습니다."
+fi
