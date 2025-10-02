27897a9cb9f1ade6e3088ceb224c40c947514dc0
sahyunjin <tkguswls1106@naver.com>
2025-10-03 01:28:37 +0900

    [#10] Feat: JVM Heap 퍼센트 필드 추가

--- src/main/java/com/shj/onlinememospringproject/controller/BackOfficeController.java
+++ src/main/java/com/shj/onlinememospringproject/controller/BackOfficeController.java
@@ -85,8 +85,8 @@ public class BackOfficeController {
 
     @GetMapping("/memory/heap")
     @Operation(summary = "힙메모리 사용량 조회 [JWT O]")
-    public ResponseEntity<ResponseData<Map<String, String>>> getHeapMemoryUsage() {
-        Map<String, String> heapMemoryMap = new LinkedHashMap<>();
+    public ResponseEntity<ResponseData<Map<String, Object>>> getHeapMemoryUsage() {
+        Map<String, Object> heapMemoryMap = new LinkedHashMap<>();
         Runtime runtime = Runtime.getRuntime();
 
         double limitMaxMB = runtime.maxMemory() / MB_DIVISOR;  // 설정된 JVM 한도의 최대 힙메모리 (한계치)
@@ -99,6 +99,7 @@ public class BackOfficeController {
         heapMemoryMap.put("maxHeap", String.format("100%% (%.2fMB · %.2fGB)", limitMaxMB, limitMaxMB/1024));  // 최대
         heapMemoryMap.put("usedHeap", String.format("%.2f%% (%.2fMB · %.2fGB)", usedPercent, usedMB, usedMB/1024));  // 사용
         heapMemoryMap.put("remainHeap", String.format("%.2f%% (%.2fMB · %.2fGB)", remainMB*100/limitMaxMB, remainMB, remainMB/1024));  // 잔여
+        heapMemoryMap.put("usedHeapPercent", usedPercent);  // Double 자료형 (이외 String)
 
         return ResponseData.toResponseEntity(ResponseCode.READ_MEMORY, heapMemoryMap);
     }
