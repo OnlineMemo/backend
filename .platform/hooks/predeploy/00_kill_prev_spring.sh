22b258269f62c179af25394bc8a42beac90d68a5
sahyunjin <tkguswls1106@naver.com>
2025-10-22 03:55:12 +0900

    [#12] Feat: AWS EB 내 nginx 불필요 로깅 방지

--- .platform/hooks/predeploy/00_kill_prev_spring.sh
+++ .platform/hooks/predeploy/00_kill_prev_spring.sh
@@ -5,7 +5,7 @@ timestamp() {
 }
 
 if pkill -f "onlinememo-backend.jar"; then
-    echo "$(timestamp) / SUCCESS - Spring 프로세스를 성공적으로 종료했습니다."
+    echo "* $(timestamp) / SUCCESS - Spring 프로세스를 성공적으로 종료했습니다."
 else
-    echo "$(timestamp) / WARN - 실행 중인 Spring 프로세스가 없어 종료하지 못했습니다."
+    echo "* $(timestamp) / WARN - 실행 중인 Spring 프로세스가 없어 종료하지 못했습니다."
 fi
--- .platform/hooks/predeploy/99_cleanup_nginx.sh
+++ .platform/hooks/predeploy/99_cleanup_nginx.sh
@@ -16,10 +16,10 @@ done
 
 if [ $IS_ERROR -eq 0 ]; then
     if sudo systemctl reload nginx; then
-        echo "$(timestamp) / SUCCESS - nginx 확장 옵션을 성공적으로 적용했습니다."
+        echo "* $(timestamp) / SUCCESS - nginx 확장 옵션을 성공적으로 적용했습니다."
     else
-        echo "$(timestamp) / ERROR - nginx 확장 옵션을 적용하지 못했습니다."
+        echo "* $(timestamp) / ERROR - nginx 확장 옵션을 적용하지 못했습니다."
     fi
 else
-    echo "$(timestamp) / ERROR - nginx 확장 옵션을 적용하지 못했습니다."
+    echo "* $(timestamp) / ERROR - nginx 확장 옵션을 적용하지 못했습니다."
 fi
